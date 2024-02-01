b1f4579a131843aa4a77299e33ac14ab82e7d422
DeepaliJangid <151712176+DeepaliJangid@users.noreply.github.com>
2024-02-01 11:24:16 +0530

    added routing paths

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './Components/login/login.component';
+import { SignupComponent } from './Components/signup/signup.component';
+import { AboutComponent } from './Components/about/about.component';
+import { NavbarComponent } from './Components/navbar/navbar.component';
+import { FetchedUsersComponent } from './Components/fetched-users/fetched-users.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'home', pathMatch: 'full' },
+  {path: 'login', component:LoginComponent, title:'Login'},
+  {path:'signup',component:SignupComponent, title:"Signup"},
+  {path:'about',component:AboutComponent, title:"About"},
+  {path:'navbar',component:NavbarComponent, title:"Navbar"},
+  {path:'fetchedusers',component:FetchedUsersComponent, title:"FetchedUsers"},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
