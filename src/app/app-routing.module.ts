65110894183dfb1c40e2e4ccc6fc5fdce93a9e16
DeepaliJangid <151712176+DeepaliJangid@users.noreply.github.com>
2024-02-02 11:29:37 +0530

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,15 +6,18 @@ import { AboutComponent } from './Components/about/about.component';
 import { NavbarComponent } from './Components/navbar/navbar.component';
 import { FetchedUsersComponent } from './Components/fetched-users/fetched-users.component';
 import { AdduserComponent } from './Components/adduser/adduser.component';
+import { authGuard } from './auth.guard';
+import { HomeComponent } from './Components/home/home.component';
 
 const routes: Routes = [
   {path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path:'home', component:HomeComponent},
   {path: 'login', component:LoginComponent, title:'Login'},
   {path:'signup',component:SignupComponent, title:"Signup"},
   {path:'about',component:AboutComponent, title:"About"},
   {path:'navbar',component:NavbarComponent, title:"Navbar"},
   {path:'fetchedusers',component:FetchedUsersComponent, title:"FetchedUsers"},
-  {path:'adduser',component:AdduserComponent}
+  {path:'adduser',component:AdduserComponent ,canActivate:[authGuard]}
 ];
 
 @NgModule({
